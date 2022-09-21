e9eedfe894a16847cbdcf99b40aa77e7fcfaedff
Dustin Cassiday <dustincassiday@gmail.com>
2022-09-21 00:54:10 -0400

    added ng-bootstrap

--- libs/web/shell/ui/src/lib/web-shell-ui.module.ts
+++ libs/web/shell/ui/src/lib/web-shell-ui.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LayoutComponent } from './components/layout/layout.component';
+import { RouterModule } from '@angular/router';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule, NgbModule],
   declarations: [LayoutComponent],
   exports: [LayoutComponent],
 })
