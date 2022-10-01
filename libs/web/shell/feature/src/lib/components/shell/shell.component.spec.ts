9ab437e31d5f735ce896731e86758da7179ba057
Dustin Cassiday <dustincassiday@gmail.com>
2022-09-30 20:44:38 -0400

    added tests

--- libs/web/shell/feature/src/lib/components/shell/shell.component.spec.ts
+++ libs/web/shell/feature/src/lib/components/shell/shell.component.spec.ts
@@ -1,7 +1,6 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 
 import { ShellFacade } from '@d13/web/shell/shared';
-import { of } from 'rxjs';
 import { ShellComponent } from './shell.component';
 
 describe('ShellComponent', () => {
@@ -13,13 +12,38 @@ describe('ShellComponent', () => {
       login: jest.fn(),
       logout: jest.fn(),
       signup: jest.fn(),
-      dismissModals: jest.fn(),
+      openModal: jest.fn(),
+      dismissAllModals: jest.fn(),
     } as any;
     sut = new ShellComponent(facade);
   });
 
   it('should create', () => {
     expect(sut).toBeTruthy();
+  });
+
+  describe('handleBtnClick', () => {
+    it('should call facade.logout with id "logout"', () => {
+      sut.handleBtnClick('logout');
+      expect(facade.logout).toHaveBeenCalled();
+    });
+
+    it('should call facade.dismissAllModals', () => {
+      sut.handleBtnClick('any id');
+      expect(facade.dismissAllModals).toHaveBeenCalled();
+    });
+
+    it('should call facade.opneModal with signupRef with id "signup"', () => {
+      sut.signupRef = {} as any;
+      sut.handleBtnClick('signup');
+      expect(facade.openModal).toHaveBeenCalledWith(sut.signupRef);
+    });
+
+    it('should call facade.opneModal loginRef with id "login"', () => {
+      sut.loginRef = {} as any;
+      sut.handleBtnClick('login');
+      expect(facade.openModal).toHaveBeenCalledWith(sut.loginRef);
+    });
   });
 
   describe('handleLoginFormSubmit', () => {
