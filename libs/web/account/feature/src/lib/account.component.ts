96ee431d125c1d60353a89124326e482b8901345
Dustin Cassiday <dustincassiday@gmail.com>
2022-10-01 23:36:39 -0400

    added titles

--- libs/web/account/feature/src/lib/account.component.ts
+++ libs/web/account/feature/src/lib/account.component.ts
@@ -1,4 +1,5 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { SeoService } from '@d13/shared/data-access';
 
 @Component({
   selector: 'd13-account',
@@ -6,4 +7,8 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   styles: [],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AccountComponent {}
+export class AccountComponent {
+  constructor(private readonly _seoService: SeoService) {
+    this._seoService.setTitle('D13 Account');
+  }
+}
